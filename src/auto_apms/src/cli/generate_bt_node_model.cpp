55e9b4bdd0ec1b92252764d36ffd9e8bc4b1703a
Robin Müller <robin-m-99@gmx.de>
2024-09-12 11:36:37 +0200

    Make api reference more comprehensible

--- src/auto_apms/src/cli/generate_bt_node_model.cpp
+++ src/auto_apms/src/cli/generate_bt_node_model.cpp
@@ -12,11 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/**
- * @file
- * @brief Command line tool to generate a model of specific behavior tree node plugins
- */
-
 #include <filesystem>
 #include <fstream>
 #include <iostream>
